051679d5cf2e29a19a7831b1749c848b3bbc1dfd
Hanny Paz <hwpazz@gmail.com>
2025-04-25 14:00:45 -0500

    se realizo algunos cambios y se agrego estilos con librerias como Lucide-react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,52 @@
+import { ShoppingBag, ShoppingCart } from "lucide-react";
+
 export default function Header() {
   return (
     <header className="bg-gray-800 py-4 px6">
-      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-        <h1 className="text-xl font-bold text-gray-800">ROPA</h1>
-        <nav className="space-x-4">
-          <a href="" className="text-gray-600 hover:text-blue-500">
-            Inicio
+      <div className="flex items-center justify-between max-w-7xl mx-auto flex-wrap gap-4">
+        {/* Menú de navegación */}
+        <nav className="flex flex-wrap space-x-6 text-white text-1xl">
+          <a href="#home" className=" hover:text-amber-300">
+            Home
           </a>
-          <a href="" className="text-gray-600 hover:text-blue-500">
-            Producto
+          <a href="#gallery" className="  hover:text-amber-300">
+            Gellery
           </a>
-          <a href="" className="text-gray-600 hover:text-blue-500">
-            Contacto
+          <a href="#shop" className=" hover:text-amber-300">
+            Shop
+          </a>
+          <a href="#contact" className=" hover:text-amber-300">
+            Contact
           </a>
         </nav>
+
+        {/* Buscador */}
+        <div className="flex items-center gap-4 flex-grow justify-center sm:justify-end">
+          <div className="hidden sm:flex items-center bg-white rounded-full px-4 py-1 w-64">
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-grow outline-none text-sm placeholder-gray-600"
+            />
+            <button className="btn-buscador text-white bg-orange-500 rounded-full p-1">
+              🔍
+            </button>
+          </div>
+
+          {/* Carrito de comparas */}
+          <div className="text-white relative cursor-pointer hover:text-amber-300">
+            <ShoppingCart size={24} />
+            {/* Contador (opcional) */}
+            <span className="absolute -top-2 -right-2 bg-red-500 text-xs text-white rounded-full px-1">
+              2
+            </span>
+          </div>
+        </div>
+
+        {/* Logo */}
+        <div className="text-white text-sm ml-2">
+          <span>Logo</span>
+        </div>
       </div>
     </header>
   );
