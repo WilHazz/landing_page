d6dc36d3b030a0c2234d2e7fe1e59a2778a7d578
Hanny Paz <hwpazz@gmail.com>
2025-06-04 13:17:13 -0500

    Vamos agregar Botones solo para el modo mobile

--- src/components/Descuento.jsx
+++ src/components/Descuento.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useSwipeable } from "react-swipeable";
 
@@ -101,10 +101,31 @@ const productosDesc = [
   },
 ];
 
-export default function Descuento({ darkMode }) {
-  const [startIndex, setStartIndex] = useState(0);
+// Hooks para visibilidad responsiva
+function useResponsiveCount() {
+  const [count, setCount] = useState(5);
 
-  const visibleCount = 5;
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 640) setCount(1); //Mobile
+      else if (width <= 768) setCount(2); //tablet
+      else if (width < 1024) setCount(3); //laptop
+      else if (width < 1280) setCount(4); //desktop
+      else setCount(5); //desktop grande
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return count;
+}
+
+export default function Descuento({ darkMode }) {
+  const visibleCount = useResponsiveCount();
+  const [startIndex, setStartIndex] = useState(0);
 
   const handleNext = () => {
     if (startIndex + visibleCount >= productosDesc.length) {
@@ -152,81 +173,86 @@ export default function Descuento({ darkMode }) {
           {/* Botones de navegación */}
           <button
             onClick={handlePrev}
-            className="absolute left-0 top-1/2 -translate-y-1/2 bg-black text-white p-2 rounded-full z-10 hover:bg-amber-600"
+            className="hidden sm:block absolute left-0 top-1/2 -translate-y-1/2 bg-black text-white p-2 rounded-full z-10 hover:bg-amber-600"
           >
             <ChevronLeft className="cursor-pointer" />
           </button>
 
           <div
             {...swipeHandlers}
-            className="cursor-pointer flex overflow-hidden gap-4"
+            className={`cursor-pointer flex overflow-hidden gap-4 ${
+              visibleCount === 1 ? "justify-center" : " "
+            } `}
           >
-            {productosDesc.slice(startIndex, startIndex + 5).map((prod) => (
-              <div
-                key={prod.id}
-                className="relative w-[300px] flex-shrink-0 group"
-              >
-                <img
-                  src={prod.imagen}
-                  alt={prod.nombre}
-                  className="w-full h-[400px] object-cover rounded-lg shadow"
-                />
-                <div className="absolute top-2 right-2 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded">
-                  -50%
-                </div>
+            {productosDesc
+              .slice(startIndex, startIndex + visibleCount)
+              .map((prod) => (
+                <div
+                  key={prod.id}
+                  className="relative w-[250px] sm:w-[300px] flex-shrink-0 group"
+                >
+                  <img
+                    src={prod.imagen}
+                    alt={prod.nombre}
+                    className="w-full h-[300px] sm:h-[400px] object-cover rounded-lg shadow"
+                  />
+                  <div className="absolute top-2 right-2 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded">
+                    -50%
+                  </div>
 
-                <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition flex items-end justify-center pointer-events-none">
-                  <div className="w-full px-4 pb-4 pointer-events-auto max-w-[200px]">
-                    <details className="w-full text-center">
-                      <summary
-                        className={`cursor-pointer hover:bg-amber-500 w-full px-3 py-1 rounded-t-full font-semibold ${
+                  <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition flex items-end justify-center pointer-events-none">
+                    <div className="w-full px-4 pb-4 pointer-events-auto max-w-[200px]">
+                      <details className="w-full text-center">
+                        <summary
+                          className={`cursor-pointer hover:bg-amber-500 w-full px-3 py-1 rounded-t-full font-semibold ${
+                            darkMode
+                              ? "bg-white text-black"
+                              : "bg-gray-800 text-white"
+                          }`}
+                        >
+                          Seleccionar talla
+                        </summary>
+                        <div className="mt-2 flex flex-wrap justify-center gap-2">
+                          {getTallas(prod.tipo).map((talla, idx) => (
+                            <span
+                              key={idx}
+                              className="bg-white text-black px-2 py-1 rounded text-sm shadow cursor-pointer hover:bg-amber-500"
+                            >
+                              {talla}
+                            </span>
+                          ))}
+                        </div>
+                      </details>
+                      <button
+                        className={`px-4 py-1 rounded-b-full font-semibold hover:bg-amber-500 transition w-full mt-[-2px] cursor-pointer ${
                           darkMode
                             ? "bg-white text-black"
                             : "bg-gray-800 text-white"
                         }`}
                       >
-                        Seleccionar talla
-                      </summary>
-                      <div className="mt-2 flex flex-wrap justify-center gap-2">
-                        {getTallas(prod.tipo).map((talla, idx) => (
-                          <span
-                            key={idx}
-                            className="bg-white text-black px-2 py-1 rounded text-sm shadow cursor-pointer hover:bg-amber-500"
-                          >
-                            {talla}
-                          </span>
-                        ))}
-                      </div>
-                    </details>
-                    <button
-                      className={`px-4 py-1 rounded-b-full font-semibold hover:bg-amber-500 transition w-full mt-[-2px] cursor-pointer ${
-                        darkMode
-                          ? "bg-white text-black"
-                          : "bg-gray-800 text-white"
-                      }`}
-                    >
-                      Añadir
-                    </button>
+                        Añadir
+                      </button>
+                    </div>
+                  </div>
+                  <div className="mt-2 text-center">
+                    <p className="font-semibold">{prod.nombre}</p>
+                    <div className="flex items-center justify-center gap-2 mt-1">
+                      <span className="text-sm text-gray-500 line-through">
+                        {prod.precioAntes}
+                      </span>
+                      <span className="text-sm text-red-600 font-bold">
+                        {prod.precioActual}
+                      </span>
+                    </div>
+                    {/* Botones para Estilo Mobile */}
                   </div>
                 </div>
-                <div className="mt-2 text-center">
-                  <p className="font-semibold">{prod.nombre}</p>
-                  <div className="flex items-center justify-center gap-2 mt-1">
-                    <span className="text-sm text-gray-500 line-through">
-                      {prod.precioAntes}
-                    </span>
-                    <span className="text-sm text-red-600 font-bold">
-                      {prod.precioActual}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))}
           </div>
 
           <button
             onClick={handleNext}
-            className="absolute right-0 top-1/2 -translate-y-1/2 bg-black text-white p-2 rounded-full z-10 hover:bg-amber-600"
+            className="hidden sm:block absolute right-0 top-1/2 -translate-y-1/2 bg-black text-white p-2 rounded-full z-10 hover:bg-amber-600"
           >
             <ChevronRight className="cursor-pointer" />
           </button>
