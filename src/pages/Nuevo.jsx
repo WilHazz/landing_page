69e04e3a2adc1658d650e4ae085793e752aef169
Hanny Paz <hwpazz@gmail.com>
2025-07-09 16:46:32 -0500

    cambiar el color para el Checkbox

--- src/pages/Nuevo.jsx
+++ src/pages/Nuevo.jsx
@@ -15,7 +15,7 @@ const productoModa = [
 const categorias = ["Camisas", "Camisetas", "Buzos", "Polos", "Chaquetas"];
 const generos = ["Mujer", "Hombre"];
 const talla = ["M", "S", "L", "XL"];
-const Manga = ["Larga", "Corta"];
+const mangas = ["Larga", "Corta"];
 
 export default function Nuevo({ darkMode }) {
   const [filtroCategoria, setFiltroCategoria] = useState([]);
@@ -60,7 +60,7 @@ export default function Nuevo({ darkMode }) {
               <div className="mt-2 spaace-y-1">
                 {categorias.map((cat) => (
                   <label key={cat} className="flex items-center">
-                    <input type="checkbox" className="mr-2" />
+                    <input type="checkbox" className="mr-2 accent-amber-400" />
                     {cat}
                   </label>
                 ))}
@@ -80,8 +80,52 @@ export default function Nuevo({ darkMode }) {
               <div className="mt-2 space-y-1">
                 {generos.map((gen) => (
                   <label key={gen} className="flex items-center">
-                    <input type="checkbox" className="mr-2" />
+                    <input type="checkbox" className="mr-2 accent-amber-400" />
                     {gen}
+                  </label>
+                ))}
+              </div>
+            )}
+          </div>
+
+          {/* Talas  */}
+          <div>
+            <div
+              className="flex justify-between items-center cursor-pointer"
+              onClick={() => toggleFiltro("talla")}
+            >
+              <h3 className="font-bold">Talla</h3>
+              <span>{openFiltros.talla ? "-" : "+"}</span>
+            </div>
+            {openFiltros.talla && (
+              <div className="mt-2 space-y-1">
+                {talla.map((t) => (
+                  <label key={t} className="flex items-center">
+                    <input type="checkbox" className="mr-2 accent-amber-400" />
+                    {t}
+                  </label>
+                ))}
+              </div>
+            )}
+          </div>
+
+          {/* Manga */}
+
+          <div>
+            <div
+              className="flex justify-between items-center cursor-pointer"
+              onClick={() => toggleFiltro("manga")}
+            >
+              <h3 className="font-bold">Manga</h3>
+              <span>{openFiltros.manga ? "−" : "+"}</span>
+            </div>
+            {openFiltros.manga && (
+              <div className="mt-2 space-y-1">
+                {mangas.map((m) => (
+                  <label key={m} className="flex items-center ">
+                    <input type="checkbox" className="mr-2 accent-amber-400" />{" "}
+                    //uso el accent para que cambie de color el checkbox
+                    {m}
                   </label>
                 ))}
               </div>
